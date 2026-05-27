bbe18d33aaa29193a7cded5c7fc6133d57854efc
chris42766 <chris42766@gmail.com>
2026-05-27 14:46:05 +0800

    update on heatlh system

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,10 +7,12 @@ public class HealthBar : MonoBehaviour
     public Sprite healthLost;
     public Sprite healthRegain;
     public bool healthEmpty=false;
+    public Animator healthanim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         health =GetComponent<SpriteRenderer>();
+        //healthanim.enabled = false;
     }
 
     // Update is called once per frame
@@ -22,10 +24,13 @@ public class HealthBar : MonoBehaviour
     {
         health.sprite = healthLost;
         healthEmpty = true;
+        healthanim.SetBool("isAnimating",true);
+        //heathanim.enabled = true;
     }
     public void AddHealth()
     {
         health.sprite = healthRegain;
         healthEmpty =false;
+        healthanim.SetBool("isAnimating", false);
     }
 }
